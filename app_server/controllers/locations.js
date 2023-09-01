75dbfe2d865c18b7749f0e396765d40d28b34118
PullaSaiKiran <131663292+PullaSaiKiran@users.noreply.github.com>
2023-09-01 10:31:13 +0530

    h

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,12 @@
+const homelist = (req,res) =>{
+    res.render('index',{title:"Home Page"});
+};
+const locationInfo = (req,res) =>{
+    res.render('index',{title:"Location Info"});
+};
+const addReview = (req,res) =>{
+    res.render('index',{title:"Review page"});
+};
+module.exports = {
+    homelist,locationInfo,addReview
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router.get('/about', ctrlOthers.about);
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 module.exports = router;
