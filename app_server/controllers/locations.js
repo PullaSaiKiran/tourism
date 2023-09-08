6121b37c24f0c3a18fb98b7347fbf993458d5f95
PullaSaiKiran <131663292+PullaSaiKiran@users.noreply.github.com>
2023-09-08 15:48:47 +0530

    hi

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,12 +1,17 @@
-const homelist = (req,res) =>{
-    res.render('index',{title:"Home Page"});
-};
-const locationInfo = (req,res) =>{
-    res.render('index',{title:"Location Info"});
-};
-const addReview = (req,res) =>{
-    res.render('index',{title:"Review page"});
-};
-module.exports = {
-    homelist,locationInfo,addReview 
-};
+const homelist = (req, res) => {
+    res.render('locations-list', { title: 'Home' });
+  };
+  
+  const locationInfo = (req, res) => {
+    res.render('location-info', { title: 'Location Info' });
+  };
+  
+  const addReview = (req, res) => {
+    res.render('location-review-form', { title: 'Add review' });
+  };
+  
+  module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+  };
